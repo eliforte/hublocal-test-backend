59133d228005e91eadad51c74a39a20f13869198
Elias Forte <eliasforte.dev@gmail.com>
2022-10-27 02:44:50 -0300

    feat: created class ErrorHandler

---
+++ src/middlewares/errors/index.ts
@@ -0,0 +1,19 @@
+import { NextFunction as Next, Request as Req, Response as Res } from 'express';
+import { ValidationError } from 'joi';
+import { IError } from '../../utils/interfaces/IErrors';
+
+interface IErrorResponse extends ValidationError, IError {}
+
+export default class ErrorHandler {
+  public static handler = (err: IErrorResponse, req: Req, res: Res, next: Next) => {
+    if (err.isJoi) {
+      return res.status(400).json({ message: err.details[0].message });
+    }
+
+    if (err.status) {
+      return res.status(err.status).json({ message: err.message });
+    }
+
+    return res.status(500).json({ message: err.message });
+  };
+}
