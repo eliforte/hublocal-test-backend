18413baa33cfff88eb24443ca9b503dec269e4b8
Elias Forte <eliasforte.dev@gmail.com>
2022-10-26 23:22:33 -0300

    feat: created initial mock

---
+++ tests/mocks/index.ts
@@ -0,0 +1,35 @@
+export const signIn = {
+  email: "hublocal@gptw.com",
+  password: "senhasenha"
+}
+
+export const signUp = {
+  name: "João",
+  email: "joão@hublocal.com",
+  password: "senhasenha",
+  is_admin: false,
+}
+
+export const createdNewClient = {
+  company: {
+    name: 'E-tech',
+    cnpj: '12.345.678/9101-11',
+    description: 'Loja de manutenção de aparelhos Apple. Autorizada da Apple',
+  },
+  places: {
+    name: 'E-tech Meireles',
+    address: 'Rua Deputado Moreira Rocha',
+    address_number: 1256,
+    complement: '',
+    cep: '60160-060',
+  },
+  responsibles: {
+    full_name: 'Carla Mendes Silva',
+    address: 'Rua Silva Paulet',
+    phone_number: '(85) 99991-9999',
+    address_number: 883,
+    complement: '',
+    cep: '60120-021',
+    is_main_responsable: false,
+  },
+}
