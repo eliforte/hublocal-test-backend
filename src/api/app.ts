bf8eae522ad056cd571d535b7a86f7d666ac71f9
Elias Forte <eliasforte.dev@gmail.com>
2022-10-26 22:20:24 -0300

    feat: created class App

---
+++ src/api/app.ts
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+export default class App {
+  public _app: express.Application;
+
+  constructor() {
+    this._app = express();
+    this._app.use(express.json());
+  }
+
+  public server(port: string | number): void {
+    this._app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  }
+
+  public newRoutes(router: express.Router): void {
+    this._app.use(router);
+  }
+
+  public errorHandler(middleware: express.ErrorRequestHandler): void {
+    this._app.use(middleware);
+  }
+
+  public useCors(options: cors.CorsOptions): void {
+    this._app.use(cors(options));
+  }
+}
