6e36a5622937eb442bb1855dfedcf1a90ee349da
Elias Forte <eliasforte.dev@gmail.com>
2022-10-27 16:54:33 -0300

    feat: add routes into app

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,18 @@
 import dotenv from 'dotenv';
 import App from './app';
+import LoginRouter from '../routes/loginRoute';
+import UserRouter from '../routes/userRoute';
+import ErrorHandler from '../middlewares/errors';
 
 dotenv.config();
 
 const app = new App();
-const corsOptions = { origin: [`${process.env.PROD_CLIENT}`, 'http://localhost:3000'] };
+const corsOptions = { origin: [`${process.env.PROD_CLIENT}`, 'http://localhost:3000', 'http://localhost:9090'] };
 
 app.useCors(corsOptions);
+app.newRoutes(new UserRouter().router);
+app.newRoutes(new LoginRouter().router);
+
+app.errorHandler(ErrorHandler.handler);
 
 export default app;
