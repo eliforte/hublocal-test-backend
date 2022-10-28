e5ccc7d87fcd75a2cefd2c078fe1a3c6aad8f9b4
Elias Forte <eliasforte.dev@gmail.com>
2022-10-28 19:09:26 -0300

    feat: create new message error

--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -14,12 +14,20 @@ export const USER_NOT_ADMIN = new NewError(401, 'Ação retrista a administrador
 
 export const ERROR_CREATING_USER = new NewError(400, 'Erro ao registrar o usuário. Aguarde um momento e tente de novamente.');
 
-export const COMPANY_EXIST = new NewError(401, 'Empresa já registrada');
+export const COMPANY_EXIST = new NewError(409, 'Empresa já registrada');
 
-export const RESPONSABLE_EXIST = new NewError(401, 'Responsável já registrado');
+export const RESPONSABLE_EXIST = new NewError(409, 'Responsável já registrado');
 
 export const ERROR_CREATING_COMPANY = new NewError(400, 'Erro ao registrar o empresa. Aguarde um momento e tente de novamente.');
 
-export const PLACE_EXIST = new NewError(401, 'Local já registrado');
+export const PLACE_EXIST = new NewError(409, 'Local já registrado');
 
 export const ERROR_CREATING_PLACE = new NewError(400, 'Erro ao registrar o estabelecimento. Aguarde um momento e tente de novamente.');
+
+export const TICKET_EXIST = new NewError(409, 'Já existe um ticket com status PENDENTE ou PROGRESSO registrado para esse estabelecimento. Verifique aba de Tickets.');
+
+export const ERROR_CREATING_TICKET = new NewError(409, 'Erro ao registrar novo ticket. Aguarde um momento e tente de novamente.');
+
+export const USER_NOT_AUTHORIZATION = new NewError(401, 'Usuário não autorizado');
+
+export const ERROR_UPDATE_TICKET = new NewError(409, 'Erro ao atualizar ticket. Aguarde um momento e tente de novamente.');
