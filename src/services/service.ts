4fcc7bf3660404663698ee3a7f5838798a264ba1
Elias Forte <eliasforte.dev@gmail.com>
2022-10-28 13:52:23 -0300

    fix: service functions params

--- src/services/service.ts
+++ src/services/service.ts
@@ -7,13 +7,13 @@ export default abstract class Service<T> {
     this._model = model;
   }
 
-  public abstract create(data: T, user_id: string | undefined): Promise<T | string>;
+  public abstract create(data: T, user_id?: string | undefined): Promise<T | string>;
 
   public abstract getAll(): Promise<T[]>;
 
   public abstract getById(id: string): Promise<T | null>;
 
-  public abstract update(id: string, data: T): Promise<T | null>;
+  public abstract update(id: string, data: T, user_id: string | undefined): Promise<T | null>;
 
   public abstract delete(id: string): Promise<T | null>;
 }
