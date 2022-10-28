45c9ccd44560ba13b81a1b2296b7c59525b54c5d
Elias Forte <eliasforte.dev@gmail.com>
2022-10-28 19:09:10 -0300

    fix: ticket schema format

--- src/utils/schemas/ticketSchema.ts
+++ src/utils/schemas/ticketSchema.ts
@@ -1,13 +1,20 @@
 import Joi from 'joi';
 
 export const SCHEMATickets = Joi.object({
-  status: Joi.string().required(),
-  name: Joi.string().min(3).required(),
-  address: Joi.string().required(),
-  address_number: Joi.number().required(),
-  upgradable_by_user: Joi.string().required(),
-  complement: Joi.string(),
-  cep: Joi.string().required(),
+  ticket: Joi.object({
+    status: Joi.string().required(),
+    title: Joi.string().required(),
+    upgradable_by_user: Joi.string().required(),
+  }),
+  place: Joi.object({
+    id: Joi.string().required(),
+    name: Joi.string().min(3),
+    address: Joi.string(),
+    phone_number: Joi.string(),
+    address_number: Joi.number(),
+    complement: Joi.string(),
+    cep: Joi.string(),
+  }),
 });
 
 export const SCHEMAUpdateTickets = Joi.object({
@@ -16,6 +23,6 @@ export const SCHEMAUpdateTickets = Joi.object({
   address: Joi.string(),
   address_number: Joi.number(),
   upgradable_by_user: Joi.string(),
-  complement: Joi.string(),
+  complement: Joi.string().allow(''),
   cep: Joi.string(),
 });
